31e5909464c1495a9f44b7b2b38c8a6fc8673fbf
PandaMan12321 <pandaman12321@local.dev>
2026-07-22 22:09:31 -0400

    Include pmi632

--- drivers/gpu/drm/panel/msm8953-generated/panel-motorola-ocean-622-tianma.c
+++ drivers/gpu/drm/panel/msm8953-generated/panel-motorola-ocean-622-tianma.c
@@ -189,7 +189,8 @@ static int tianma_622_v0_probe(struct mipi_dsi_device *dsi)
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
-			  MIPI_DSI_MODE_LPM;
+			  MIPI_DSI_MODE_LPM | MIPI_DSI_CLOCK_NON_CONTINUOUS;
+
 
 	ctx->panel.prepare_prev_first = true;
 
